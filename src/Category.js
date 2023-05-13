ba8e1e710c89117609acf4e6e5142a6b5a27ca92
meetujpanchal <meet46884@gmail.com>
2023-05-13 13:04:11 +0530

    add

--- src/Category.js
+++ src/Category.js
@@ -8,30 +8,104 @@ export class Category extends Component {
         super(props);
 
         this.state = {
-            categories: []            
+            categories: [],
+            modalTitle: "",
+            CategoryId: 0,
+            Categoryname: "",
+            Description: "",
+            Image: "",
+            ActiveStatus: ""
         }
 
     }
 
-    refreshList(){
-        fetch(variables.API_URL+'CategoryTbls')
-        .then(responce=>responce.json())
-        .then(data=>{
-            this.setState({categories:data});
-        })        
+    refreshList() {
+        fetch(variables.API_URL + 'CategoryTbls')
+            .then(responce => responce.json())
+            .then(data => {
+                this.setState({ categories: data });
+            })
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.refreshList();
+    }
+
+    changeCategoryName = (e) => {
+        this.setState({ Categoryname: e.target.value });
+    }
+
+    addClick() {
+        this.setState({
+            modalTitle: "Add Category",
+            CategoryId: 0,
+            Categoryname: "",
+            Description: "",
+            Image: "",
+            ActiveStatus: ""
+        });
+    }
+
+    createClick() {
+        fetch(variables.API_URL + 'CategoryTbls', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                Categoryname: this.state.Categoryname
+            })
+        })
+            .then(res => res.json())
+            .then((result) => {
+                alert(result);
+                this.refreshList();
+            }, (error) => {
+                alert('Failed');
+            })
+    }
+
+    deleteClick(id) {
+        if (window.confirm('Are you sure?')) {
+            fetch(variables.API_URL + 'CategoryTbls/' + id, {
+                method: 'DELETE',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(res => res.json())
+                .then((result) => {
+                    alert(result);
+                    this.refreshList();
+                }, (error) => {
+                    alert('Data deleted!!')
+                })
+        }
     }
 
     render() {
         const {
-            categories,            
-        }=this.state;
+            categories,
+            modalTitle,
+            CategoryId,
+            Categoryname,
+            Description,
+            Image,
+            ActiveStatus
+        } = this.state;
 
         return (
+
             <div className="mt-5 d-flex justify-content-left">
+                <button type="button"
+                    className="btn btn-primary m-2 float-end"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
+                    onClick={() => this.addClick()}>
+                    Add Category
+                </button>
                 <table className="table table-striped">
                     <thead>
                         <th>
@@ -48,11 +122,11 @@ export class Category extends Component {
                         </th>
                         <th>
                             ActiveStatus
-                        </th>                        
+                        </th>
                         <th>
                             Options
                         </th>
-                    </thead>                    
+                    </thead>
                     <tbody>
                         {categories.map(dep =>
                             <tr key={dep.CategoryId}>
@@ -60,7 +134,7 @@ export class Category extends Component {
                                 <td>{dep.Categoryname}</td>
                                 <td>{dep.Description}</td>
                                 <td>{dep.Image}</td>
-                                <td>{dep.ActiveStatus}</td>                                
+                                <td>{dep.ActiveStatus}</td>
                                 <td>
                                     <button type="button"
                                         className="btn btn-light mr-1">
@@ -70,7 +144,8 @@ export class Category extends Component {
                                         </svg>
                                     </button>
                                     <button type="button"
-                                        className="btn btn-light mr-1">
+                                        className="btn btn-light mr-1"
+                                        onClick={() => this.deleteClick(dep.CategoryId)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                                             <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
                                         </svg>
@@ -79,7 +154,49 @@ export class Category extends Component {
                             </tr>
                         )}
                     </tbody>
-                </table>                
+                </table>
+                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-hidden="true">
+                    <div className="modal-dialog modal-lg modal-dialog-centered">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">{modalTitle}</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"
+                                ></button>
+                            </div>
+
+                            <div className="modal-body">
+                                <div className="input-group mb-3">
+                                    <span className="input-group-text">CategoryName</span>
+                                    <input type="text" className="form-control"
+                                        value={Categoryname}
+                                        onChange={this.changeCategoryName} />
+                                </div>
+                                <div className="input-group mb-3">
+                                    <span className="input-group-text">Description</span>
+                                    <input type="text" className="form-control"
+                                        value={Categoryname}
+                                        onChange={this.changeCategoryName} />
+                                </div>
+
+                                {CategoryId == 0 ?
+                                    <button type="button"
+                                        className="btn btn-primary float-start"
+                                        onClick={() => this.createClick()}
+                                    >Create</button>
+                                    : null}
+
+                                {CategoryId != 0 ?
+                                    <button type="button"
+                                        className="btn btn-primary float-start"
+                                        onClick={() => this.updateClick()}
+                                    >Update</button>
+                                    : null}
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
